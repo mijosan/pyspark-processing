e3f9b00710d11f4b208b8336b4a7441cd9f41b76
ChoiTaesan <mijosan@naver.com>
2022-07-26 17:37:09 +0900

    feat: dataframe col ex

---
+++ df2.py
@@ -0,0 +1,16 @@
+from pyspark.sql import SparkSession
+
+schema = "Id INT, First STRING,Last STRING, Uri STRING, Published STRING, Hits INT, Campaigns ARRAY<STRING>"
+
+data = [[1,  "Jules",  "Damji",  "https://tiny나rl.l", "1/4/2016",  4535,  ["twitter", "Linkedln"]], [2,  "Jules",  "Damji",  "https://tiny나rl.l", "1/4/2016",  4535,  ["twitter", "Linkedln"]], [3,  "Jules",  "Damji",  "https://tiny나rl.l", "1/4/2016",  4535,  ["twitter", "Linkedln"]]]
+
+spark = (SparkSession
+         .builder
+         .appName("PythonMnMCount")
+         .getOrCreate())
+
+blogs_df = spark.createDataFrame(data, schema)
+
+# blogs_df.show()
+
+print(blogs_df.printSchema())
--- mmcount.py
+++ mmcount.py
@@ -8,10 +8,22 @@ spark = (SparkSession
          .getOrCreate())
 
 # 명령행 인자에서 M&M 데이터가 들어 있는 파일 이름을 얻는다.
-mnm_file = sys.argv[1]
+mnm_file = "C:/Users/mobigen/running-spark/data/mnm_dataset.csv"
 
 mnm_df = (spark.read.format("csv")
           .option("header", "true")
           .option("inferSchema", "true")
           .load(mnm_file))
 
+count_mnm_df = (mnm_df
+                .select("State", "Color", "Count")
+                .groupBy("State", "Color")
+                .sum("Count")
+                .orderBy("sum(Count)", ascending=False))
+
+count_mnm_df.show(n=60, truncate=False)
+print("Total Rows = %d" % (count_mnm_df.count()))
+
+
+while True:    # while에 True를 지정하면 무한 루프
+    print('Hello, world!')

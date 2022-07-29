5ca1bda4d75ae2ac42fca5f3914cbda1139236af
ChoiTaesan <mijosan@naver.com>
2022-07-29 16:39:49 +0900

    feat: struct streamming ex1 dev

---
+++ struct_stream_ex1.py
@@ -0,0 +1,24 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+
+spark = (SparkSession
+         .builder
+         .appName("struct_streamming")
+         .getOrCreate())
+
+lines = (spark
+         .readStream.format("socket")
+         .option("host", "192.168.100.73")
+         .option("port", 9999)
+         .load())
+
+words = lines.select(split(col("value"), "\\s").alias("word"))
+
+checkpointDir = "C:/Users/mobigen/running-spark/result"
+
+streamingQuery = (words
+          .writeStream.format("console")
+          .outputMode("append")
+          .trigger(processingTime="1 second")
+          .option("checkpointLocation", checkpointDir)
+          .start().awaitTermination())

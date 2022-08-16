b8961d4320dafd7d82ced823271cb9d4fb717185
ChoiTaesan <mijosan@naver.com>
2022-08-16 14:18:15 +0900

    feat: collect vs count

--- mmcount.py
+++ mmcount.py
@@ -17,13 +17,8 @@ mnm_df = (spark.read.format("csv")
 
 count_mnm_df = (mnm_df
                 .select("State", "Color", "Count")
-                .groupBy("State", "Color")
-                .sum("Count")
-                .orderBy("sum(Count)", ascending=False))
+                .collect())
 
+print(count_mnm_df)
 count_mnm_df.show(n=60, truncate=False)
 print("Total Rows = %d" % (count_mnm_df.count()))
-
-
-while True:    # while에 True를 지정하면 무한 루프
-    print('Hello, world!')
